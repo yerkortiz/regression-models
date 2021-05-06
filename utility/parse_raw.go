bdc285d0157bca2be027779e5228368b97d496b9
Yerko <yerko.ortizmora@gmail.com>
2021-05-05 20:33:59 -0400

    feat: group by time interval

--- utility/parse_raw.go
+++ utility/parse_raw.go
@@ -4,81 +4,98 @@ import (
 	"bufio"
 	"encoding/csv"
 	"encoding/json"
-	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"time"
 )
 
-func main() {
-	/*
-		fileName := os.Args[1]
-		country := os.Args[2]
-		disasterType := os.Args[3]
-		outPath := os.Args[4]
-	*/
-	fileName := "/Users/yerko/codes/twitter-traffic-predict/raw_data/example.jsonl"
-	country := "Chile"
-	disasterType := "Earthquake"
-	outPath := "/Users/yerko/codes/twitter-traffic-predict/datasets"
-	outFileName := "dataset.csv"
-	fmt.Println("Parsing:", fileName)
-	file, err := os.Open(fileName)
+type twitData struct {
+	CreatedAt string `json:"created_at"`
+	ID        int    `json:"id"`
+	Unix      int64
+}
+type parsedData struct {
+	Unix         int64
+	Country      string
+	DisasterType string
+	Quantity     int
+}
+
+func evalErr(err error) {
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
+}
+func readRaw(fileName string) []twitData {
+	file, err := os.Open(fileName)
+	evalErr(err)
 	defer file.Close()
-	type twitData struct {
-		CreatedAt    string `json:"created_at"`
-		ID           int    `json:"id"`
-		Unix         int64
-		Country      string
-		DisasterType string
-	}
 	var result []twitData
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		var obj twitData
 		err := json.Unmarshal(json.RawMessage(line), &obj)
-		if err != nil {
-			log.Println(err)
-		}
+		evalErr(err)
 		date, err := time.Parse(time.RubyDate, obj.CreatedAt)
-		if err != nil {
-			log.Println(err)
-		}
+		evalErr(err)
 		obj.Unix = date.Unix()
-		obj.Country = country
-		obj.DisasterType = disasterType
 		result = append(result, obj)
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	var parsedResult [][]string
-	//headerCsv := []string{"Country", "Unix", "Disaster", "ID"}
-	//parsedResult = append(parsedResult, headerCsv)
+	err = scanner.Err()
+	evalErr(err)
+	file.Close()
+	return result
+}
+func parseRaw(result []twitData, country string, disasterType string) []parsedData {
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Unix < result[j].Unix
+	})
+	current := (result[0].Unix - result[0].Unix%60)
+	var resultGrouped []parsedData
+	c := 0
 	for _, value := range result {
+		if current > value.Unix {
+			c++
+		} else {
+			temp := parsedData{current, country, disasterType, c}
+			resultGrouped = append(resultGrouped, temp)
+			c = 0
+			current += 60
+		}
+	}
+	resultGrouped = append(resultGrouped, parsedData{current, country, disasterType, c})
+	return resultGrouped
+}
+func writeCsv(parsedData []parsedData, outFilePath string) {
+	var parsedResult [][]string
+	for _, value := range parsedData {
 		var temp []string
 		temp = append(temp, value.Country)
 		temp = append(temp, strconv.FormatInt(value.Unix, 10))
 		temp = append(temp, value.DisasterType)
-		temp = append(temp, strconv.Itoa(value.ID))
+		temp = append(temp, strconv.Itoa(value.Quantity))
 		parsedResult = append(parsedResult, temp)
 	}
-	fileCsv, err := os.Create(outPath + "/" + outFileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	fileCsv, err := os.Create(outFilePath)
+	evalErr(err)
+	defer fileCsv.Close()
 	writer := csv.NewWriter(fileCsv)
 	defer writer.Flush()
 	for _, value := range parsedResult {
 		err := writer.Write(value)
-		if err != nil {
-			log.Fatal(err)
-		}
+		evalErr(err)
 	}
+
+}
+func main() {
+	inFilePath := "/Users/yerko/codes/twitter-traffic-predict/raw_data/nepal_2015.jsonl"
+	country := "Nepal"
+	disasterType := "Earthquake"
+	outFilePath := "/Users/yerko/codes/twitter-traffic-predict/datasets/dataset.csv"
+	result := readRaw(inFilePath)
+	resultGrouped := parseRaw(result, country, disasterType)
+	writeCsv(resultGrouped, outFilePath)
 }
